4e71234ccd08c124f811ff1a1eb84e816b8f4bce
Choux30 <kmerschoff@gmail.com>
2026-05-20 21:02:26 -0500

    Set up 30-day Python journey repo

---
+++ day_01_variables_input /about_me.py
@@ -0,0 +1,13 @@
+print("Welcome to my About Me app!")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+dream_job = input("What is your dream job? ")
+
+print()
+print("--- PROFILE ---")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Favorite Hobby: {hobby}")
+print(f"Dream Job: {dream_job}")
